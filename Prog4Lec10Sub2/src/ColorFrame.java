a4665b695b6715fcc3c172b29a8eb32c5accdaee
Tsubasa <kametsun7@gmail.com>
2024-06-04 16:18:17 +0900

    [add] 課題E

---
+++ Prog4Lec10Sub2/src/ColorFrame.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ColorFrame extends JFrame implements ActionListener {
+    private final Panel panel;
+
+    public ColorFrame() {
+        setTitle("Prog4Lec10Sub2");
+        setSize(350, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.panel = new Panel();
+
+        Button redButton = new Button("赤");
+        Button blueButton = new Button("青");
+        Button greenButton = new Button("緑");
+        Button yellowButton = new Button("黄色");
+
+        redButton.addActionListener(this);
+        blueButton.addActionListener(this);
+        greenButton.addActionListener(this);
+        yellowButton.addActionListener(this);
+
+        panel.add(redButton);
+        panel.add(blueButton);
+        panel.add(greenButton);
+        panel.add(yellowButton);
+
+        add(panel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String colorString = e.getActionCommand();
+
+        switch (colorString) {
+            case "赤":
+                panel.setBackground(Color.RED);
+                break;
+            case "青":
+                panel.setBackground(Color.BLUE);
+                break;
+            case "緑":
+                panel.setBackground(Color.GREEN);
+                break;
+            case "黄色":
+                panel.setBackground(Color.YELLOW);
+                break;
+        }
+    }
+}
